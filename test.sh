da2878cd0419e88e7cc66c52820a5fdbb4f52984
Takatoshi MATSUO <matsuo.tak@gmail.com>
2021-09-26 16:44:17 +0900

    improve test script

--- test.sh
+++ test.sh
@@ -2,88 +2,77 @@
 
 RESULT=""
 
-# unit test
+function run_testcase {
+    echo
+    echo "############################## run $TESTNAME test ################################"
+    echo "go run . $OPTIONS $INPUT > $OUTPUT"
+    go run . $OPTIONS $INPUT > $OUTPUT
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echo "** $TESTNAME test Failed (exit $rc)**"
+        RESULT="${RESULT}${TESTNAME} test failed (exit $rc)\n"
+    else
+        echo "------- diff ---------------------------"
+        diff -urN $WANT $OUTPUT
+        rc=$?
+        echo "----------------------------------------"
+        if [ $rc -ne 0 ]; then
+            echo "** $TESTNAME Failed **"
+            RESULT="${RESULT}${TESTNAME} test failed\n"
+        else
+            echo "** $TESTNAME test OK **"
+        fi
+    fi
+}
 
+
+# unit test
 echo "############################## run unit test #################################"
 go test -v || RESULT="Unit test failed\n"
 
+
 # test Merge
-echo
-echo "############################## run merge test ################################"
+TESTNAME="merge"
 INPUT="testcase/Dockerfile.before-merge"
 WANT="testcase/Dockerfile.after-merge"
 OUTPUT="testcase/_tmp_Dockerfile.test-merge"
-
-go run . -m $INPUT > $OUTPUT
-echo "------- diff ---------------------------"
-diff -urN $WANT $OUTPUT
-rc=$?
-echo "----------------------------------------"
-if [ $rc -ne 0 ]; then
-    echo "** merge test Failed **"
-    RESULT="${RESULT}Merge test failed\n"
-else
-    echo "** merge test OK **"
-fi
+OPTIONS="-m"
+run_testcase
 
 
 # test Split
-echo
-echo "############################## run split test ################################"
+TESTNAME="split"
 INPUT="testcase/Dockerfile.before-split"
 WANT="testcase/Dockerfile.after-split"
 OUTPUT="testcase/_tmp_Dockerfile.test-split"
+OPTIONS="-s"
+run_testcase
 
-go run . -s $INPUT > $OUTPUT
-echo "------- diff ---------------------------"
-diff -urN $WANT $OUTPUT
-rc=$?
-echo "----------------------------------------"
-if [ $rc -ne 0 ]; then
-    echo "** split test Failed **"
-    RESULT="${RESULT}Split test failed\n"
-else
-    echo "** split test OK **"
-fi
 
 # test Include
-echo
-echo "############################## run include test ################################"
+TESTNAME="include"
 INPUT="testcase/Dockerfile.before-include"
 WANT="testcase/Dockerfile.after-include"
 OUTPUT="testcase/_tmp_Dockerfile.test-include"
+OPTIONS="-i"
+run_testcase
 
-go run . -i $INPUT > $OUTPUT
-echo "------- diff ---------------------------"
-diff -urN $WANT $OUTPUT
-rc=$?
-echo "----------------------------------------"
-if [ $rc -ne 0 ]; then
-    echo "** include test Failed **"
-    RESULT="${RESULT}Include test failed\n"
-else
-    echo "** include test OK **"
-fi
 
 # test Include and Merge
-echo
-echo "############################## run include and merge test ################################"
+TESTNAME="include and merge"
 INPUT="testcase/Dockerfile.before-include-and-merge"
 WANT="testcase/Dockerfile.after-include-and-merge"
 OUTPUT="testcase/_tmp_Dockerfile.include-and-merge"
+OPTIONS="-i -m"
+run_testcase
 
-go run . -i -m $INPUT > $OUTPUT
-echo "------- diff ---------------------------"
-diff -urN $WANT $OUTPUT
-rc=$?
-echo "----------------------------------------"
-if [ $rc -ne 0 ]; then
-    echo "** include and merge test Failed **"
-    RESULT="${RESULT}Include and Merge test failed\n"
-else
-    echo "** include and merge test OK **"
-fi
 
 echo
-echo -e "############## Failed tests is below #################\n$RESULT"
+if [ "$RESULT" != "" ]; then
+    echo -e "############## Results #################\n$RESULT"
+    exit 1
+else
+    echo -e "############## Results #################"
+    echo -e "All tests are successful"
+fi
 
